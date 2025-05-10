38ac540ceb75643868f06597e08627ab710a7fa8
Alex Navarro <alex@navarrotech.net>
2025-05-10 15:02:19 -0600

    Github Actions and Github Pages

---
+++ README.md
@@ -0,0 +1,5 @@
+# Algorivm Homepage
+
+This is the homepage that will go onto https://www.algorivm.com
+
+It will be hosted with Github Pages
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// Copyright © 2025 Algorivm
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    // For GitHub Pages:
+    outDir: 'docs',
+  }
+})
