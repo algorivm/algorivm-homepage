51963bf5c64f9f7f349cb866185fea887c994a0e
Alex Navarro <alex@navarrotech.net>
2025-06-19 21:04:02 -0600

    Team roles

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
 // Copyright © 2025 Algorivm
+
+for (const grid of document.querySelectorAll('.image-grid')) {
+  function removeActiveImages() {
+    grid.querySelectorAll('img.is-active').forEach(img => {
+      img.classList.remove('is-active')
+    })
+    grid.classList.remove('has-active')
+  }
+
+  for (const image of grid.querySelectorAll('img')) {
+    image.addEventListener('click', () => {
+      if (image.classList.contains('is-active')) {
+        removeActiveImages()
+      } else {
+        removeActiveImages()
+        image.classList.add('is-active')
+        grid.classList.add('has-active')
+      }
+    })
+  }
+
+  for (const spoiler of grid.querySelectorAll('.spoiler')) {
+    spoiler.addEventListener('click', () => {
+      removeActiveImages()
+    })
+  }
+}
